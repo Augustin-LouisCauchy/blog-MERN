c413cd5dd2d1ccf9d900498f8ee11f62150ee850
Mihail Nechaev <sadovni4ek@gmail.com>
2024-05-27 19:03:29 +0300

    validation for Model

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import { registerValidation } from "./validations/auth.js";
 
 mongoose
   .connect(
@@ -13,25 +14,8 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: "Jason Statham",
-    },
-    "secret123"
-  );
-
-  res.json({
-    success: true,
-    token,
-  });
+app.post("/auth/register", registerValidation, (req, res) => {
+  
 });
 
 app.listen(4444, (err) => {
