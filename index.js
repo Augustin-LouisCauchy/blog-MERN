628d52a77af1c8251e9178a42706534187343eab
Mihail Nechaev <sadovni4ek@gmail.com>
2024-05-27 20:48:20 +0300

    JWT token && try catch added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from "express";
+import express, { json } from "express";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { validationResult } from "express-validator";
 import { registerValidation } from "./validations/auth.js";
@@ -8,7 +8,7 @@ import UserModel from "./models/User.js";
 
 mongoose
   .connect(
-    "mongodb+srv://John_Smith:ORmaxE3rXq0lHeou@cluster0.pbjkzzl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://John_Smith:ORmaxE3rXq0lHeou@cluster0.pbjkzzl.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => console.log("DB OK"))
   .catch((err) => console.log("DB error", err));
@@ -17,26 +17,46 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, async(req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password; // Вытаскиваю пароль из body перед созданием документа (doc)
+    const salt = await bcrypt.genSalt(10); //генерация строки символов
+    const passwordHash = await bcrypt.hash(password, salt); //шифрование пароля с помощью алгоритма salt
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await doc.save(); // сохраняю в базе
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d" // токен будет не валидным через 30 дней
+      }
+    );
+
+    res.json({
+      ...user,
+      token
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось зарегистрироваться",
+    });
   }
-
-  const password = req.body.password   // Вытаскиваю пароль из body перед созданием документа (doc)
-  const salt = await bcrypt.genSalt(10)  //генерация строки символов
-  const passwordHash = await bcrypt.hash(password, salt) //шифрование пароля с помощью алгоритма salt
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash: req.body.passwordHash,
-  })
-
-  res.json({
-    success: true,
-  });
 });
 
 app.listen(4444, (err) => {
