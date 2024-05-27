c1af7bd00796bf0f1216588039c60f2fabe4dccb
Mihail Nechaev <sadovni4ek@gmail.com>
2024-05-27 19:38:05 +0300

    password encryption added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
 import mongoose from "mongoose";
+import { validationResult } from "express-validator";
 import { registerValidation } from "./validations/auth.js";
+import UserModel from "./models/User.js";
 
 mongoose
   .connect(
@@ -14,8 +17,26 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
-  
+app.post("/auth/register", registerValidation, async(req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  const password = req.body.password   // Вытаскиваю пароль из body перед созданием документа (doc)
+  const salt = await bcrypt.genSalt(10)  //генерация строки символов
+  const passwordHash = await bcrypt.hash(password, salt) //шифрование пароля с помощью алгоритма salt
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash: req.body.passwordHash,
+  })
+
+  res.json({
+    success: true,
+  });
 });
 
 app.listen(4444, (err) => {
