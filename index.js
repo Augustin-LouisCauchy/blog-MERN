d5d28e559ad552b7b468e0bb63160850541969b8
Mihail Nechaev <sadovni4ek@gmail.com>
2024-05-29 21:47:09 +0300

    folder controllers && first file added

--- index.js
+++ index.js
@@ -62,49 +62,7 @@ app.post("/auth/login", async (req, res) => {
   }
 });
 
-app.post("/auth/register", registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password; // Вытаскиваю пароль из body перед созданием документа (doc)
-    const salt = await bcrypt.genSalt(10); //генерация строки символов
-    const hash = await bcrypt.hash(password, salt); //шифрование пароля с помощью алгоритма salt
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-
-    const user = await doc.save(); // сохраняю в базе
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      {
-        expiresIn: "30d", // токен будет не валидным через 30 дней
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Не удалось зарегистрироваться",
-    });
-  }
-});
+app.post("/auth/register", registerValidation);
 
 app.get("/auth/me", checkAuth, async (req, res) => {
   try {
